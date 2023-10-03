bb190a33d797ed35176f2fe2f3631cc784ff4cfa
isaka <c0b220090b@edu.teu.ac.jp>
2023-10-03 16:48:48 +0900

    演習問題1と2の途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,15 @@ delta = {
     pg.K_RIGHT: (+5, 0),
 }
 
+left_total_mv = [(-5, +5), (-5, 0), (-5, -5)] 
+right_total_mv = [
+    (0,-5), (+5, -5), (+5, 0), 
+    (+5, +5),(0, +5),
+]
+
+accs = [a for a in range(1, 11)]
+
+
 def check_bound(obj_rct: pg.Rect):
     """
     引数: 引数はこうかとん、または爆弾のRect
@@ -30,11 +39,21 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    """こうかとん"""
+    """こうかとん"""    
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900, 400)
+    #turn_kk_img = pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 90, 0)
+    #kk_img_dct = {}
+    #for i, num in enumerate(left_total_mv, -1):
+    #    kk_img_dct[num] = pg.transform.rotozoom(kk_img, 45*i, 0)
+    #for i, num in enumerate(right_total_mv):
+    #    kk_img_dct[num] = pg.transform.rotozoom(turn_kk_img, 45*i, 0)
+    #kk_rct_dct = {}
+    #for i, key, value in enumerate(kk_img_dct.items()):
+    #    kk_rct_dct[key] = value.get_rect()
+    #    kk_rct_dct[key].center = (900, 400)
     """爆弾"""
     bd_img = pg.Surface((20, 20))#練習1
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
@@ -42,6 +61,17 @@ def main():
     bd_rct = bd_img.get_rect()
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bd_rct.center = (x, y)
+    #bb_imgs = []
+    #for r in range(1, 11):
+    #    bb_img = pg.Surface((20*r, 20*r))
+    #    pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+    #    bb_img.set_colorkey((0, 0, 0))
+    #    bb_imgs.appned(bb_img)
+    #bb_rcts = []
+    #for datum in bb_imgs:
+    #    bb_rcts.append(datum.get_rect())
+    #    datum.center = (x, y)
+    #bd_rct.center = (x, y)
     vx, vy = +5, +5 #練習2
 
     clock = pg.time.Clock()
@@ -65,9 +95,13 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+                #kk_img_new = kk_img_dct[(sum_mv[0], sum_mv[1])]
+                #kk_rct_new = kk_rct_dct[(sum_mv[0], sum_mv[1])]
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+            #kk_img_new = kk_img_dct[(sum_mv[0], sum_mv[1])]
+            #kk_rct_new = kk_rct[(sum_mv[0], sum_mv[1])]
         screen.blit(kk_img, kk_rct)
         """爆弾"""
         bd_rct.move_ip(vx, vy)
@@ -76,6 +110,8 @@ def main():
             vx *= -1
         if not tate:
             vy *= -1
+        #avx, avy = vx*accs[min(tmr//500, 9)], vy*accs[min(tmr//500, 9)]
+        #bb_img = bb_imgs[min(tmr//500, 9)]
         screen.blit(bd_img, bd_rct)
 
         pg.display.update()
